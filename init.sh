e3fb491fbe9cd32c5426978407368bd71f4d4fea
Dan Adrian Mirea <damirea84@gmail.com>
2024-08-30 23:41:07 +0300

    remove init.sh

--- init.sh
+++
@@ -1,8 +0,0 @@
-echo "# RayReflectBalls" >> README.md
-git init
-git add README.md
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/RayReflectBalls.git
-git push -u origin main
